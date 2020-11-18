b477b6ea94c527205ddb625db9fee1097ebd0633
Gabriel Ginn Suarez <ginnlaiho@gmail.com>
2020-11-18 14:30:00 -0600

    add user validatiion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+            length: { minimum: 3, maximum: 25 },
+            uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
 end
